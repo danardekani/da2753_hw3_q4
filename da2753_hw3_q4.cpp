bc288a5d28de9420a96fc244a8c3f74b6edfcb8f
Daniel <ardekanicapital@gmail.com>
2020-07-30 02:07:53 -0400

    ask user for real number, ask them to select rounding method, round there answer. Complete

--- da2753_hw3_q4.cpp
+++ da2753_hw3_q4.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <math.h>
 using namespace  std;
 
-int main()
-{
-    int realNumber;
+const int FLOOR_ROUND = 1;
+const int CEILING_ROUND = 2;
+const int ROUND = 3;
 
+int main()
+{;
+    int roundingMethod;
+    double realNumber;
+
+    //User input
     cout << "Please enter a Real number:" << endl;
     cin >> realNumber;
-    return 0
+
+    //Choose rounding method
+    cout << "Choose your rounding method:" << endl;
+    cout << "1. Floor round" << endl;
+    cout << "2. Ceiling round" << endl;
+    cout << "3. Round to the nearest whole number" << endl;
+    cin >> roundingMethod;
+
+    switch(roundingMethod)
+    {
+        case FLOOR_ROUND:
+            cout << floor(realNumber) << endl;
+            break;
+        case CEILING_ROUND:
+            cout << ceil(realNumber) << endl;
+            break;
+        case ROUND:
+            cout << round(realNumber) << endl;
+            break;
+    }
+
+    return 0;
 }
